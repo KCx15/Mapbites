0c673bf3e1f44c93f9794c3f39a429bf4a7920ce
KCx15 <scroll558user@gmail.com>
2026-01-06 16:44:45 +0000

    Add restaurant resource routes

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CuisineController;
+use App\Http\Controllers\RestaurantController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('cuisines', CuisineController::class);
+Route::resource('restaurants', RestaurantController::class);
+Route::get('/', fn () => redirect()->route('restaurants.index'));
