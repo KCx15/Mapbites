550085d0eba18009078cd11259f5a5a3e953ef6b
KCx15 <scroll558user@gmail.com>
2026-01-08 13:02:41 +0000

    Fix MapController imports and load cuisines correctly

--- app/Http/Controllers/MapController.php
+++ app/Http/Controllers/MapController.php
@@ -2,20 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cuisine;     
+use App\Models\Restaurant;
 use Illuminate\Http\Request;
 
 class MapController extends Controller
 {
-  public function index()
-{
-    $restaurants = \App\Models\Restaurant::with('cuisine')
-        ->whereNotNull('lat')->whereNotNull('lng')
-        ->orderBy('name')
-        ->get();
+ public function index(Request $request)
+    {
+        $cuisines = Cuisine::orderBy('name')->get();
 
-    $cuisines = \App\Models\Cuisine::orderBy('name')->get();
+        $query = Restaurant::with('cuisine')
+            ->whereNotNull('lat')
+            ->whereNotNull('lng');
 
-    return view('map.index', compact('restaurants', 'cuisines'));
-}
+        if ($request->filled('cuisine')) {
+            $query->whereHas('cuisine', function ($q) use ($request) {
+                $q->where('slug', $request->cuisine);
+            });
+        }
+
+        $restaurants = $query->orderBy('name')->get();
+
+        return view('map.index', compact('restaurants', 'cuisines'));
+    }
 
 }
